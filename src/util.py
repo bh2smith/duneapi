d75b0429ecafb13771db7065ebd56e4188381e0e
Ben Smith <bh2smith@gmail.com>
2022-03-19 10:53:36 +0100

    add function type annotation

--- src/util.py
+++ src/util.py
@@ -1,8 +1,9 @@
 """Utility methods to support Dune API"""
 from datetime import datetime
+from typing import Any
 
 
-def datetime_parser(dct):
+def datetime_parser(dct: dict[str, Any]) -> dict[str, Any]:
     """
     Used as object hook in json loads method to parse postgres dates strings
     """

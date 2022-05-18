1ed233c5cb7db034f6aee0ef335a04850a529d5e
Ben Smith <bh2smith@gmail.com>
2022-05-18 16:42:08 +0200

    trying to restore backup and query from python

--- tests/db/test_db.py
+++ tests/db/test_db.py
@@ -8,38 +8,38 @@ class TestMockDB(unittest.TestCase):
         db_conn = connect()
         cur = db_conn.cursor()
 
-        table = """
-            CREATE TABLE student(
-                id SERIAL PRIMARY KEY, 
-                firstName VARCHAR(40) NOT NULL, 
-                lastName VARCHAR(40) NOT NULL, 
-                age INT, 
-                address VARCHAR(80), 
-                email VARCHAR(40)
-            )
-        """
-        values = [
-            "Ben",
-            "Smith",
-            37,
-            "Berlin, Germany",
-            "bh2smith@gmail.com",
-        ]
+        # table = """
+        #     CREATE TABLE student(
+        #         id SERIAL PRIMARY KEY, 
+        #         firstName VARCHAR(40) NOT NULL, 
+        #         lastName VARCHAR(40) NOT NULL, 
+        #         age INT, 
+        #         address VARCHAR(80), 
+        #         email VARCHAR(40)
+        #     )
+        # """
+        # values = [
+        #     "Ben",
+        #     "Smith",
+        #     37,
+        #     "Berlin, Germany",
+        #     "bh2smith@gmail.com",
+        # ]
 
-        insert = """
-            INSERT INTO student(firstname, lastname, age, address, email) 
-            VALUES(%s, %s, %s, %s, %s) RETURNING *
-        """.format(
-            values
-        )
+        # insert = """
+        #     INSERT INTO student(firstname, lastname, age, address, email) 
+        #     VALUES(%s, %s, %s, %s, %s) RETURNING *
+        # """.format(
+        #     values
+        # )
 
-        cur.execute(table)
-        cur.execute(insert, values)
+        # cur.execute(table)
+        # cur.execute(insert, values)
         # Its weird the way values is used twice here!
 
-        cur.execute("SELECT * FROM student")
+        cur.execute("SELECT count(*) FROM erc20.tokens")
         x = cur.fetchall()
-        self.assertEqual(1, len(x))
+        self.assertEqual(x[0], 6363)
 
 
 if __name__ == "__main__":
